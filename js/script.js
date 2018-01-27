abce113a5cb151f1a923faf4a82a7fdd28104094
Marta Waplak <mwaplak@gmail.com>
2018-01-27 11:18:47 +0100

    Add new project

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var a = prompt('Enter base value'),
+    h = prompt('Enter height value'),
+    triangleArea = a*h/2,
+    result = alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+                    
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
